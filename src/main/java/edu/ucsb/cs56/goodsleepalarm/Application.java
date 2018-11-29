65f4b0a807c2f105fe63e7385a08acc4b7450e08
jakrami <justinakrami@gmail.com>
2018-11-28 16:59:05 -0800

    Update Application.java

--- src/main/java/edu/ucsb/cs56/goodsleepalarm/Application.java
+++ src/main/java/edu/ucsb/cs56/goodsleepalarm/Application.java
@@ -2,8 +2,10 @@ package edu.ucsb.cs56.goodsleepalarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class Application {
     public static void main(String[] args) {
 	  SpringApplication.run(Application.class, args);
